f04d17e0543173ca2e684892f61a1ea7239d47d2
Oran Rush <oranrush@Orans-MBP.lan1>
2019-05-03 12:39:52 -0500

    Upload2

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -8,7 +8,53 @@ class Saved extends React.Component {
     };
 
     componentDidMount() {
-        this.loadBooks();
+        this.uploadedBooks();
+    }
+
+    uploadedBooks = () => {
+        API.getBooks ().then(res => {
+            this.setState({
+                books: res.data
+            })
+        }).catch(err => {
+            console.log(err);
+        });
+
+        deletedBooks = (id) => {
+            API.deletedBooks(id).then(res => {
+                this.uploadedBooks();
+            }).catch(err => {
+                console.log(err);
+            });
+
+        }
+    }
+
+    render () {
+        return (
+        <div>
+        <Jumbotron>
+            <h1>Saved Books</h1>
+        </Jumbotron>
+
+        <Container>
+            {this.state.books.length (
+                <List>
+                    {this.state.books.map(book =>
+                        <BookCard
+                        key= {index}
+                        title={book.title}
+                        authors={book.authors}
+                        description={book.description}
+                        image={book.image}
+                        link={book.link}
+                        deletedBooks={() => this.deletedBooks(book._id)}
+                        />)}
+                </List>
+            )}
+        </Container>
+        </div>
+        );
     }
 }
 
